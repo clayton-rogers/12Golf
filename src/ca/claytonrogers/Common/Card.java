a7e73ddccb9cf58df5accb8bd146470da5fa47c9
claytonrogers <claytonrogers53@gmail.com>
2015-07-05 16:01:24 -0400

    Added initial classes and planning documentation.

---
+++ src/ca/claytonrogers/Common/Card.java
@@ -0,0 +1,7 @@
+package ca.claytonrogers.Common;
+
+/**
+ * Created by clayton on 2015-07-04.
+ */
+public class Card {
+}
--- src/ca/claytonrogers/Main.java
+++
@@ -1,8 +0,0 @@
-package ca.claytonrogers;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
