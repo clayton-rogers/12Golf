62a5f70052277cdb3d4321064c1f2769d3436942
claytonrogers <claytonrogers53@gmail.com>
2015-07-10 20:22:28 -0400

    Rewrote a large portion of the Server to authenticate players, and to handle a variable number of players.
    (cherry picked from commit 79ca3b61d44bae45d88dc555b1e540792fc3efa0)

--- src/ca/claytonrogers/Server/Server.java
+++ src/ca/claytonrogers/Server/Server.java
@@ -2,6 +2,10 @@ package ca.claytonrogers.Server;
 
 import ca.claytonrogers.Common.Connection;
 import ca.claytonrogers.Common.Constants;
+import ca.claytonrogers.Common.Messages.Message;
+import ca.claytonrogers.Common.Messages.VersionInformation;
+import ca.claytonrogers.Common.Messages.VersionInformationAuthenticated;
+import ca.claytonrogers.Common.Messages.VersionInformationMismatch;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -14,18 +18,26 @@ public class Server {
     public static void main (String[] args) {
         System.out.println("Server starting...");
 
+        final int NUM_PLAYER = 2;
+
         try (ServerSocket serverSocket = new ServerSocket(Constants.PORT_NUMBER)) {
             try {
                 while (true) {
-                    System.out.println("Waiting for player 1 to connect...");
-                    Socket socket = serverSocket.accept();
-                    Connection player1 = new Connection(socket);
 
-                    System.out.println("Waiting for player 2 to connect...");
-                    socket = serverSocket.accept();
-                    Connection player2 = new Connection(socket);
+                    Connection[] players = new Connection[NUM_PLAYER];
+                    for (int i = 0; i < NUM_PLAYER; i++) {
+                        System.out.println("Waiting for player " + i + " to connect...");
+                        Socket socket = serverSocket.accept();
+                        players[i] = new Connection(socket);
+                    }
 
-                    GameRunner gameRunner = new GameRunner(new Connection[]{player1, player2});
+                    if (!authenticate(players)) {
+                        for (Connection player : players) {
+                            player.close();
+                        }
+                        continue;
+                    }
+                    GameRunner gameRunner = new GameRunner(players);
                     gameRunner.start();
                 }
             } catch (IOException e) {
@@ -34,5 +46,35 @@ public class Server {
         } catch (IOException e) {
             System.out.println("Problem starting the server.");
         }
+    }
+
+    /**
+     * Authenticates the given list of players, returning true if all players authenticated
+     * successfully.
+     *
+     * @param players The list of players to be authenticated.
+     * @return True when all players are authenticated, false if any one of them fails.
+     */
+    private static boolean authenticate (Connection[] players) {
+        for (int i = 0; i < players.length; i++) {
+            Connection player = players[i];
+
+            Message message = player.waitForNextMessage();
+            if (message.getMessageType() != Message.MessageType.VersionInformation) {
+                System.out.println("Received something other than version info for first message: " + message.getMessageType());
+                return false;
+            }
+
+            int clientVersion = ((VersionInformation)message).getVersionNumber();
+            if (clientVersion != Constants.VERSION) {
+                System.out.println("Client has different version number");
+                player.send(new VersionInformationMismatch(clientVersion, Constants.VERSION));
+                return false;
+            } else {
+                player.send(new VersionInformationAuthenticated(i));
+            }
+        }
+
+        return true;
     }
 }
