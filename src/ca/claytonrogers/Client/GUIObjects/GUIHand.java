63217d33d7b16805a0ce55cced653345f83fe4c1
claytonrogers <claytonrogers53@gmail.com>
2015-07-11 20:09:53 -0400

    Implemented the drawing of a hand.

--- src/ca/claytonrogers/Client/GUIObjects/GUIHand.java
+++ src/ca/claytonrogers/Client/GUIObjects/GUIHand.java
@@ -1,5 +1,6 @@
 package ca.claytonrogers.Client.GUIObjects;
 
+import ca.claytonrogers.Common.Card;
 import ca.claytonrogers.Common.Constants;
 import ca.claytonrogers.Common.Hand;
 import ca.claytonrogers.Common.IntVector;
@@ -15,11 +16,16 @@ public class GUIHand extends GUIObject {
     private int position = 0; // 0-3 for the different players
 
     public GUIHand(Hand hand, int position) {
-        super(position == 0 ? Constants.BOTTOM_HAND_OFFSET : Constants.TOP_HAND_OFFSET,
+        super(
+                getHandLocation(position),
                 Constants.HAND_SIZE,
                 Type.Hand);
         this.hand = hand;
         this.position = position;
+    }
+
+    private static IntVector getHandLocation(int position) {
+        return Constants.HAND_LOCATIONS[position];
     }
 
     public int getClickedCard(IntVector clickLocation) {
@@ -51,17 +57,31 @@ public class GUIHand extends GUIObject {
 
     @Override
     protected void internalDraw(Graphics g) {
-        switch (position) {
-            case 0:
-                // TODO draw down position
-                break;
-            case 1:
-                throw new IllegalStateException("Cannot be a player 1");
-            case 2:
-                // TODO draw top position
-                break;
-            case 3:
-                throw new IllegalStateException("Cannot be a player 3");
+        if (position == 0 || position == 2) {
+            drawHorizontalHand(g);
+        } else {
+            drawVerticalHand(g);
         }
+    }
+
+    private void drawHorizontalHand(Graphics g) {
+        final int handWidth = Constants.NUMBER_OF_CARDS/2;
+        IntVector cardLocation = location;
+        for (int i = 0; i < handWidth; i++) {
+            for (int row = 0; row < 2; row ++) {
+                Card card = hand.getCard(i + row*handWidth);
+                Drawer.drawCard(g, card, cardLocation);
+
+                // Increment the card location to the right
+                cardLocation = cardLocation.add(new IntVector(Constants.SPACE_BETWEEN_CARDS + Constants.CARD_SIZE.x,0));
+            }
+            // Increment the card location down a row
+            cardLocation = location.add(new IntVector(0,Constants.SPACE_BETWEEN_CARDS + Constants.CARD_SIZE.y));
+        }
+    }
+
+    private void drawVerticalHand(Graphics g) {
+        // TODO FUTURE
+        throw new IllegalStateException("Cannot draw vertical hands at this time.");
     }
 }
